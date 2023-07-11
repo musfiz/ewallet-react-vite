d581375e6bc8ec22aefaf4e2c3431d9426a8d3d0
Mustafizur Rahman <rahman.mustafizur@bjitgroup.com>
2023-07-11 16:57:03 +0600

    Clear app page redundant code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,14 @@
-import Login from "./layouts/Login";
 import Index from "./layouts/Index";
-import { useState, useEffect } from "react";
-import Cookies from "universal-cookie";
-const cookies = new Cookies();
+
+const Page = () => {
+  return (
+    <>
+      <Index />
+    </>
+  );
+};
 
 const App = () => {
-  const [token, setToken] = useState(false);
-  const [user, setUser] = useState({});
-
-  const Page = () => {
-    return (
-      <>
-        <Index user={user} />
-      </>
-    );
-  };
-
-  // const logout = () => {
-  //   setToken(null);
-  //   setUser({});
-  // };
-
-  // useEffect(() => {
-  //   const getUser = cookies.get("user");
-  //   const getToken = cookies.get("token");
-  //   if (!getUser || !getToken) {
-  //     logout();
-  //   }
-  //   setUser(getUser);
-  //   setToken(true);
-  // }, [token]);
-
   return (
     <>
       <Page />
